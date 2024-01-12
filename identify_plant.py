a418129f1bb779ab3a323d63fb699282d444e0f3
Kcops11 <TrekStar11@proton.me>
2024-01-12 02:12:30 -0600

    Good place to stop for the day

---
+++ identify_plant.py
@@ -0,0 +1,70 @@
+import cv2  
+from ultralytics import YOLO
+import math
+
+# load model config and weights
+net =  YOLO("yolo-Weights/yolov8n.pt")
+
+# define classes
+classes = [
+    'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+    'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+    'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
+    'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+    'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+    'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
+    'hair drier', 'toothbrush'
+]
+
+# load webcam
+cap = cv2.VideoCapture(0)
+
+# Function to Get Output Layers
+while True:
+    ret, img = cap.read()
+    results = net(img, stream=True)
+    # coordinates
+    for r in results:
+        boxes = r.boxes
+
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+            # put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+             
+
+            # class name
+            cls = int(box.cls[0])
+
+            print("-----DEBUG-----")
+            print("cls is: " , cls)
+            print("classes cls is:" , classes[cls])
+            if 0 <= cls < len(classes):
+                if classes[cls] == "potted plant":
+                    print("Confidence --->", confidence)
+                    print("Class name -->", classes[cls])
+                    # object details
+                    org = [x1, y1]
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    fontScale = 1
+                    color = (0, 0, 255)
+                    thickness = 2
+
+                    cv2.putText(img, classes[cls], org, font, fontScale, color, thickness)
+            else:
+                print("Invalid class index:", cls)
+
+    cv2.imshow('img', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
